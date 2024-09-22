445e050dcbafad92055e9510a1f2a4f0dd8aed71
IsaiahHoffman <idhoffman@wisc.edu>
2024-09-22 08:41:15 -0500

    Test

--- public/orderProcessing.js
+++ public/orderProcessing.js
@@ -1,24 +1,47 @@
+const {
+    LinkedList,
+    LinkedListNode
+  } = require('@datastructures-js/linked-list');
+
 var ticket
 var loadedOrder
+
+var myHeaders = new Headers()
+myHeaders.append('Content-Type', 'application/json')
+var data = {
+    ticket: ticket,
+}
+const URL = '/orders';
+fetch(URL, {
+    method: 'POST',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default',
+    body: JSON.stringify(data)
+}).then((res) => res.json()).then((data) => {
+    loadedOrder = data
+    bringUpOrder()
+})
+
 document.getElementById("search").onclick = function () {
     ticket = document.getElementById("ticket").value
-    
+
     var myHeaders = new Headers()
-            myHeaders.append('Content-Type', 'application/json')
+    myHeaders.append('Content-Type', 'application/json')
     var data = {
         ticket: ticket,
     }
     const URL = '/orderLookUp';
-            fetch(URL, {
-                method: 'POST',
-                headers: myHeaders,
-                mode: 'cors',
-                cache: 'default',
-                body: JSON.stringify(data)
-            }).then((res) => res.json()).then((data) => {
-                loadedOrder = data
-                bringUpOrder()
-            })
+    fetch(URL, {
+        method: 'POST',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default',
+        body: JSON.stringify(data)
+    }).then((res) => res.json()).then((data) => {
+        loadedOrder = data
+        bringUpOrder()
+    })
 }
 
 
